608399627f02abc9374f404d764761058cbbd239
wnsjs10 <wnsjsl1028@gmail.com>
2026-09-18 20:14:38 +0900

    03 수정본

--- main.c
+++ main.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
-int main(void) {
-    int a, b;
+
+    int main(void) {
+    char c;
 
     
-    printf("분자를 입력하세요 : ");
-    scanf("%d", &a);
+    printf("enter a character : ");
+    scanf("%c", &c);
 
     
-    printf("분모를 입력하세요 : ");
-    scanf("%d", &b);
+    printf("the next character of %c (%d) is %c (%d)\n", c, c, c + 1, c + 1);
 
-    
-    printf("나누기의 결과는 %f 입니다.\n", (double)a / b);
 
     return 0;
 }
